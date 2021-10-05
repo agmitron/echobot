0c2f2be0bb100f5b5f75dbde88788bc2f21cfe1c
Alexey Gmitron <gmitron01@gmail.com>
2021-10-05 20:25:23 +0700

    feat: working bot with only text

--- app/Main.hs
+++ app/Main.hs
@@ -2,16 +2,18 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module Main where
-import Prelude hiding (id)
+
+import qualified Control.Monad.IO.Class
 import Data.Aeson (FromJSON, ToJSON, decode, eitherDecode)
 import qualified Data.ByteString.Char8 as B8
 import Data.ByteString.Lazy (fromStrict, toStrict)
 import qualified Data.ByteString.Lazy.Internal as BC
-import GHC.Generics
-import Network.HTTP.Simple (Request, getResponseBody, httpBS, parseRequest_, Response)
-import Data.Function ((&))
 import Data.Either (fromRight)
-import qualified Control.Monad.IO.Class
+import Data.Function ((&))
+import GHC.Generics
+import Network.HTTP.Simple (Request, Response, getResponseBody, httpBS, parseRequest_)
+import Prelude hiding (id)
+import qualified Prelude as P (id)
 
 newtype Chat = Chat
   { id :: Integer
@@ -36,21 +38,24 @@ data TelegramResponse = TelegramResponse
   }
   deriving (Show, Generic)
 
-
 instance FromJSON Chat
+
 instance ToJSON Chat
 
 instance FromJSON Message
+
 instance ToJSON Message
 
 instance FromJSON Result
+
 instance ToJSON Result
 
 instance FromJSON TelegramResponse
+
 instance ToJSON TelegramResponse
 
 -- todo: move token to .env
-token = "1975796426:AAGiifTuCe5-PN25Uzc28omSHFxA37GmphI"
+token = ""
 
 endpoint = "https://api.telegram.org/bot" ++ token ++ "/"
 
@@ -63,7 +68,6 @@ getUpdateId (Right res) = if null results then Left "Results array is empty" els
     results = result res
 getUpdateId (Left err) = Left $ "getUpdateId error: " ++ err
 
-
 incrementOffset :: Integer -> Integer
 incrementOffset offset = offset + 1
 
@@ -74,18 +78,20 @@ getChatIdAndMessageText res = (chatId, messageText)
     chatId = msg & chat & id
     messageText = msg & text
 
-
-sendMessage (chatId, messageText) =
-  httpBS (parseRequest_ (endpoint ++ "sendMessage?chat_id=" ++ show chatId ++ "&text=" ++ show messageText))
+sendMessage :: (Control.Monad.IO.Class.MonadIO m) => (Integer, String) -> m ()
+sendMessage (chatId, messageText) = do
+  httpBS (parseRequest_ (endpoint ++ "sendMessage?chat_id=" ++ show chatId ++ "&text=" ++ P.id messageText))
+  return ()
 
 reply :: Response B8.ByteString -> IO (Either String TelegramResponse)
 reply responseJSON = do
   let updatesResponse = getResponse responseJSON
   case updatesResponse of
-        (Left err) -> putStrLn err
-        (Right response) -> do
-          responseJSON <- sendMessage (getChatIdAndMessageText response)
-          B8.putStrLn $ getResponseBody responseJSON
+    (Left err) -> putStrLn err
+    (Right response) ->
+      if null (result response)
+        then return ()
+        else sendMessage $ getChatIdAndMessageText response
   return updatesResponse
 
 checkAndReply :: Maybe Integer -> IO ()
@@ -95,11 +101,10 @@ checkAndReply offset = do
     Just x -> httpBS (parseRequest_ (endpoint ++ "getUpdates" ++ "?offset=" ++ show x))
   updatesResponse <- reply responseJSON
   case getUpdateId $ getResponse responseJSON of
-    Left err -> error err
+    Left _ -> checkAndReply Nothing
     Right updateId ->
       checkAndReply (Just $ incrementOffset updateId)
 
 main :: IO ()
 main =
   checkAndReply Nothing
-
